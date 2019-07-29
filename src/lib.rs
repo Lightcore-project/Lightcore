2225c1b4fa0cb250a6a01f310174a8d1d7dfcdf2
tiannian <dtiannian@aliyun.com>
2019-07-30 01:54:45 +0800

    test crust

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,4 @@
-extern crust;
+extern crate crust;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+mod p2p;
+
--- src/p2p.rs
+++ src/p2p.rs
@@ -1,1 +1,15 @@
 
+
+#[cfg(test)]
+mod tests {
+    use crust::Service;
+    use crust::read_config_file;
+    use std::sync::mpsc::{ channel };
+
+    #[test]
+    fn test_conn() {
+        let ( tx, rx ) = channel();
+        let service = Service::new(tx, "hello");
+    }
+}
+
