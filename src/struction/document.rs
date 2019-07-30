ff9f80f6f0160764dc2abb31757b17159a1d4554
tiannian <dtiannian@aliyun.com>
2019-07-30 16:38:56 +0800

    add document

--- src/struction/document.rs
+++ src/struction/document.rs
@@ -1,3 +1,6 @@
+use sha3::Digest;
+use sha3::Sha3_256;
+
 use crate::error::Error;
 
 pub enum DocType {
@@ -8,15 +11,22 @@ pub enum DocType {
 }
 
 pub struct Document {
+    doc_id: [u8;32],
     doctype: DocType,
     content: String,
 }
 
 impl Document {
     fn new(t: DocType, content: &str) -> Self {
+        let mut hash = <[u8;32]>::default();
+        let mut hasher = Sha3_256::new();
+        hasher.input(content);
+        hash.copy_from_slice(&hasher.result()[..]);
+
         Document {
+            doc_id: hash,
             doctype: t,
-            content: String::from(content)
+            content: String::from(content),
         }
     }
 
